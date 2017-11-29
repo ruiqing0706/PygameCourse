05fd6b662f0dbb0a4175f79a9c95a8b76e339992
ruiqing0706 <2944925501@qq.com>
2017-11-29 20:12:12 +0800

    Pygame-HelloWorld

---
+++ PYG02/PYG02-PygameHelloWorld.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+
+"""
+@author:jiruiqing
+contact:ruiqing0706@163.com
+@file: PYG02-PygameHelloWorld.py
+@time: 2017/11/29 20:05
+"""
+
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Pygame游戏之旅")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
